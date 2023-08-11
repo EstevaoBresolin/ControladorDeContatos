58b8e27f8cecfa8d50193a35b637a864e2b175fc
Estevão Bresolin <123606381+EstevaoBresolin@users.noreply.github.com>
2023-08-11 17:42:16 -0300

    Add files via upload

---
+++ Banco.cs
@@ -0,0 +1,22 @@
+﻿// Banco.cs
+using System.Collections.Generic;
+
+namespace ControleContatos.Models
+{
+    public class Banco
+    {
+        public List<ContatoModel> lista { get; set; }
+
+        public List<UsuarioModel> listaUsuario { get; set; }
+
+       
+
+        public Banco()
+        {
+            lista = new List<ContatoModel>();
+            listaUsuario = new List<UsuarioModel>();
+            
+        }
+       
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,42 @@
+﻿using ControleContatos.Models;
+
+namespace ControleContatos
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Aqui você registra o serviço BancoDados
+            services.AddSingleton<Banco>();
+            services.AddTransient<ContatoModel>();
+            services.AddTransient<GeradorDeContato>();
+            services.AddTransient<UsuarioModel>();
+            services.AddTransient<LoginModel>();
+
+            services.AddControllersWithViews();
+
+            // Outros serviços podem ser registrados aqui...
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (!env.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Login}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
